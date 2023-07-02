1874fc06bdc93927d81605e44381a061d74feb01
RicardoAymay <r.aymay.goncalves@gmail.com>
2023-07-02 14:14:03 -0300

    fix: data agora retorna em formato legível. Campo updatedAt agora é opcional para poder passar valor nulo

--- src/modules/comments/entities/comment.entity.ts
+++ src/modules/comments/entities/comment.entity.ts
@@ -1,13 +1,17 @@
 import { randomUUID } from 'crypto';
 
 export class Comment {
-    readonly id: string
-    content: string
-    userId: string
+  readonly id: string;
+  content: string;
+  userId: string;
+  createdAt: string; // Add this
+  updatedAt: string | null; // Add this
 
-    constructor(id: string, userId: string){
-        this.id = randomUUID()
-        this.userId = userId
-    }
+  constructor(userId: string, content: string) {
+    this.id = randomUUID();
+    this.userId = userId;
+    this.content = content;
+    this.createdAt = new Date().toLocaleString();
+    this.updatedAt = null;
+  }
 }
-
