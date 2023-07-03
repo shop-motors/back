8985aa3db0c75100a10131af7d4774ae21e160a0
Gabriel Ortiz <106622591+Ortiz1111@users.noreply.github.com>
2023-07-03 10:38:44 -0300

    feat: finalizando a doc da galeria

--- src/modules/gallery/dto/create-gallery.dto.ts
+++ src/modules/gallery/dto/create-gallery.dto.ts
@@ -1,7 +1,9 @@
 /* eslint-disable prettier/prettier */
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateGalleryDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   img_url: string;
